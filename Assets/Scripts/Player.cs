555e12b07db369a391de19d0daf5a7a785910f92
Vlad Spalatelu <vlad.spalatelu@gmail.com>
2022-03-31 10:02:42 +0300

    Player moving logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 }
