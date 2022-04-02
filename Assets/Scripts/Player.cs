ac1933fc84785317b31ef550774e5c3d0fe5d2eb
Vlad Spalatelu <vlad.spalatelu@gmail.com>
2022-04-02 13:17:34 +0300

    Player bounds logic + code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,54 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+        KeepPlayerInBounds();
+    }
+
+    private void PlayerMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+    }
+
+    private void KeepPlayerInBounds()
+    {
+        const float BOTTOM_MARGIN = -3.8f;
+        const float UPPER_MARGIN = 0f;
+        const float RIGHT_MARGIN = 11.3f;
+        const float LEFT_MARGIN = -RIGHT_MARGIN;
+
+        // Does not work on different screen ratios (only 16:9). Working on it
+        if (transform.position.y >= UPPER_MARGIN)
+        {
+            transform.position = new Vector3(transform.position.x, UPPER_MARGIN, transform.position.z);
+        }
+        else if (transform.position.y < BOTTOM_MARGIN)
+        {
+            transform.position = new Vector3(transform.position.x, BOTTOM_MARGIN, transform.position.z);
+        }
+
+        if (transform.position.x >= RIGHT_MARGIN)
+        {
+            transform.position = new Vector3(RIGHT_MARGIN, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < LEFT_MARGIN)
+        {
+            transform.position = new Vector3(LEFT_MARGIN, transform.position.y, transform.position.z);
+        }
+
+        // Player logic to reapper on the opposite corner when going out of bounds
+        // Silky smooth transition
+        /*if (transform.position.x >= RIGHT_MARGIN)
+        {
+            transform.position = new Vector3(LEFT_MARGIN, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < LEFT_MARGIN)
+        {
+            transform.position = new Vector3(RIGHT_MARGIN, transform.position.y, transform.position.z);
+        }*/
     }
 }
